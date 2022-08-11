dfcaf6ef58458796c02f5b0db911e3dd47a8c855
Russell Kirmayer <russellkir@gmail.com>
2022-08-11 11:29:15 -0700

    Basic update_coin endpoint started

--- coin_tracker.py
+++ coin_tracker.py
@@ -1,8 +1,51 @@
-from flask import Flask
+from flask import Flask, request, jsonify
+import time
 
 app = Flask(__name__)
+
+
+class CTMember:
+    def __init__(self, name):
+        self.name = name
+        self.wallet = Wallet()
+        self.created_at = time.now()
+
+
+class Wallet:
+    def __init__(self):
+        self.total_value = 0
+        self.created_at = time.now()
+
+
+class CoinPK:
+    def __init__(self):
+        self.blockchain = ""
+
+
+class Transaction:
+    def __init__(self, address):
+        self.created_at = time.now()
+        self.address = address
 
 
 @app.route("/")
 def hello():
     return "Hello, World!"
+
+
+@app.route("/update_coin", methods=["POST", "DELETE"])
+def update_coin():
+    content_type = request.headers.get("Content-Type")
+    if content_type != "application/json":
+        return "Content-Type not supported!"
+
+    json = request.json
+    print(json)
+    resp = jsonify(success=True)
+    if request.method == "POST":
+        # TODO: update coin address here
+        pass
+    if request.method == "DELETE":
+        # TODO: delete coin address here
+        pass
+    return resp
